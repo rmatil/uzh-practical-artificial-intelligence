6d825002941ed08d689bcc44f6eec3aa25f03648
Raphael Matile <raphael.matile@gmail.com>
2017-04-03 14:57:27 +0200

    Flee from wolf when no food items are left anymore

--- assignment01/kingsheep/kingsheep/team/rmatil/minimax/sheep/SheepMiniMax.java
+++ assignment01/kingsheep/kingsheep/team/rmatil/minimax/sheep/SheepMiniMax.java
@@ -19,7 +19,7 @@ public class SheepMiniMax extends MiniMax {
     public static final int MAX_POSITIVE_INCENTIVE = 5;
     public static final int POSITIVE_INCENTIVE     = 1;
     public static final int INDIFFERENT_INCENTIVE  = 0;
-    public static final int MAX_NEGATIVE_INCENTIVE = - 2;
+    public static final int MAX_NEGATIVE_INCENTIVE = - 6;
 
     public SheepMiniMax(Player player) {
         super(player);
@@ -66,6 +66,25 @@ public class SheepMiniMax extends MiniMax {
                     return (1f / currentState.getCurrentDepth()) * SheepMiniMax.MAX_NEGATIVE_INCENTIVE;
                 }
 
+                int wolfX = - 1;
+                int wolfY = - 1;
+                for (int y = 0; y < currentState.getMap().length; y++) {
+                    for (int x = 0; x < currentState.getMap()[0].length; x++) {
+                        if (currentState.getMap()[y][x] == this.player.getOppositeOpponentType()) {
+                            wolfX = x;
+                            wolfY = y;
+                            break;
+                        }
+                    }
+                }
+
+                int manhattanWolfDistance = Math.abs(currentState.getCurrentX() - wolfX) + Math.abs(currentState.getCurrentY() - wolfY);
+
+                if (manhattanWolfDistance <= 1) {
+                    // flee
+                    return SheepMiniMax.MAX_NEGATIVE_INCENTIVE;
+                }
+
                 if (collision.getCollisionParticipants().contains(Type.RHUBARB)) {
                     logger.info(String.format("[depth: %d][x: %d, y: %d] terminal utility: %d", currentState.getCurrentDepth(), currentState.getCurrentX(), currentState.getCurrentY(), SheepMiniMax.MAX_POSITIVE_INCENTIVE));
                     return (1f / currentState.getCurrentDepth()) * SheepMiniMax.MAX_POSITIVE_INCENTIVE;
@@ -96,7 +115,6 @@ public class SheepMiniMax extends MiniMax {
 
         int currentX = currentState.getCurrentX();
         int currentY = currentState.getCurrentY();
-        int manhattanWolfDistance;
 
         List<Integer> rhubarbDistances = new ArrayList<>();
         List<Integer> grassDistances = new ArrayList<>();
@@ -111,13 +129,9 @@ public class SheepMiniMax extends MiniMax {
                     break;
                 }
             }
-
-            if (wolfX != - 1 && wolfY != - 1) {
-                break;
-            }
         }
 
-        manhattanWolfDistance = Math.abs(currentX - wolfX) + Math.abs(currentY - wolfY);
+        int manhattanWolfDistance = Math.abs(currentX - wolfX) + Math.abs(currentY - wolfY);
 
         for (int y = 0; y < currentState.getMap().length; y++) {
             for (int x = 0; x < currentState.getMap()[0].length; x++) {
@@ -164,7 +178,8 @@ public class SheepMiniMax extends MiniMax {
         }
 
         // we choose the distance to the wolf if neither grass nor rhubarb exists
-        float incentive = calculateWolfIncentive(manhattanWolfDistance);
+        int emptySpaceAround = countEmptySpace(currentState, 2);
+        float incentive = calculateWolfIncentive(manhattanWolfDistance, emptySpaceAround);
         logger.info(String.format("[depth: %d][x: %d, y: %d][wolf] heuristic utility: %.2f", currentState.getCurrentDepth(), currentState.getCurrentX(), currentState.getCurrentY(), incentive));
         return incentive;
     }
@@ -186,7 +201,68 @@ public class SheepMiniMax extends MiniMax {
         return (1f / distance) * (1f / itemIncentive) - Math.abs((1f / wolfDistance) * (SheepMiniMax.MAX_NEGATIVE_INCENTIVE / 1f));
     }
 
-    private float calculateWolfIncentive(int wolfDistance) {
-        return (- 1f) * Math.abs((1f / wolfDistance) * (SheepMiniMax.MAX_NEGATIVE_INCENTIVE / 1f));
+    private float calculateWolfIncentive(int wolfDistance, int emptySpaceAround) {
+        //((float) emptySpaceAround)
+        return (1f / emptySpaceAround) - Math.abs((1f / wolfDistance) * (SheepMiniMax.MAX_NEGATIVE_INCENTIVE / 1f));
+    }
+
+    private int countEmptySpace(State currentState, int depth) {
+        int currentX = currentState.getCurrentX();
+        int currentY = currentState.getCurrentY();
+        int emptySpaceCounter = 0;
+
+        // count right
+        for (int i = currentX; i < Math.min(currentState.getMap()[currentY].length, currentX + depth); i++) {
+            if (currentState.getMap()[currentY][i] != Type.FENCE &&
+                    currentState.getMap()[currentY][i] != this.player.getOppositeOpponentType() &&
+                    currentState.getMap()[currentY][i] != this.player.getEqualOpponentType() &&
+                    currentState.getMap()[currentY][i] != this.player.getTeamMateType()) {
+                emptySpaceCounter++;
+            } else {
+                // there might be an item blocking our escape way
+                break;
+            }
+        }
+
+        // count left
+        for (int i = currentX; i > Math.max(- 1, currentX - depth); i--) {
+            if (currentState.getMap()[currentY][i] != Type.FENCE &&
+                    currentState.getMap()[currentY][i] != this.player.getOppositeOpponentType() &&
+                    currentState.getMap()[currentY][i] != this.player.getEqualOpponentType() &&
+                    currentState.getMap()[currentY][i] != this.player.getTeamMateType()) {
+                emptySpaceCounter++;
+            } else {
+                // there might be an item blocking our escape way
+                break;
+            }
+        }
+
+        // count upwards
+        for (int i = currentY; i < Math.min(currentState.getMap().length, currentY + depth); i++) {
+            if (currentState.getMap()[i][currentX] != Type.FENCE &&
+                    currentState.getMap()[i][currentX] != this.player.getOppositeOpponentType() &&
+                    currentState.getMap()[i][currentX] != this.player.getEqualOpponentType() &&
+                    currentState.getMap()[i][currentX] != this.player.getTeamMateType()) {
+                emptySpaceCounter++;
+            } else {
+                // there might be an item blocking our escape way
+                break;
+            }
+        }
+
+        // count downwards
+        for (int i = currentY; i > Math.max(- 1, currentY - depth); i--) {
+            if (currentState.getMap()[i][currentX] != Type.FENCE &&
+                    currentState.getMap()[i][currentX] != this.player.getOppositeOpponentType() &&
+                    currentState.getMap()[i][currentX] != this.player.getEqualOpponentType() &&
+                    currentState.getMap()[i][currentX] != this.player.getTeamMateType()) {
+                emptySpaceCounter++;
+            } else {
+                // there might be an item blocking our escape way
+                break;
+            }
+        }
+
+        return emptySpaceCounter;
     }
 }
